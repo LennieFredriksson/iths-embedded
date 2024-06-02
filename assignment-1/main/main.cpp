6418f41ecebc91900508c4ee920dbbcef97ac3d6
Lennie Fredriksson <Lennie.Fredriksson@outlook.com>
2024-06-02 22:59:43 +0200

    First try to flash code to ESP32

--- assignment-1/main/main.cpp
+++ assignment-1/main/main.cpp
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+
+static const char *TAG = "MAIN";
 
 extern "C" void app_main(void)
 {
-
+    for (size_t i = 0; i < 10; i++)
+    {
+        ESP_LOGI(TAG,"Testing");
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
 }
